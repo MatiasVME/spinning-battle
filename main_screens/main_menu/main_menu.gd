4646e305ad763a6786455b666eaf108adfdc6dc8
Matìas Valentìn Muñoz Espinoza <writkas2@gmail.com>
2025-01-30 12:52:50 -0300

    otro

--- main_screens/main_menu/main_menu.gd
+++ main_screens/main_menu/main_menu.gd
@@ -1,1 +1,10 @@
 extends Control
+
+
+func _ready() -> void:
+	if Main.is_cat_defeated:
+		$btn_cat.hide()
+	if Main.is_king_defeated:
+		$btn_king.hide()
+	if Main.is_lizard_defeated:
+		$btn_lagarto.hide()
