438e0bd2ca96b35c02e518c29c23aa56cedbfa80
Alexis Bogarin <alexisbogarin61@gmail.com>
2025-01-30 02:05:13 -0300

    Update UIs
    Actualice todo fondos y agregue la logica para ir al juego con el nuevo menui.

---
+++ main_screens/main_menu/btn_cat.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+@onready var globo = $"../globo_cat"
+
+func _ready():
+	globo.visible = false
+
+func _process(delta):
+	if get_rect().has_point(to_local(get_global_mouse_position())):
+		globo.visible = true
+	else:
+			globo.visible = false
+
+
+func _input(event):
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().change_scene_to_file("res://game/stage/stage.tscn")
